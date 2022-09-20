929375cf5e0b4051fe5cfd5dd032de1b5d0e1b4f
Kunal <108722806+Kunal0323@users.noreply.github.com>
2022-09-21 00:45:21 +0530

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM line/kubectl-kustomize:1.20.2-3.9.1
+
+COPY entrypoint.sh /bin/
+RUN chmod +x /bin/entrypoint.sh
+
+ENV SSH_KEY=
+ENV IMAGES=
+ENV IMAGE_TAG=
+ENV MANIFEST_HOST=
+ENV MANIFEST_USER=
+ENV MANIFEST_USER_EMAIL=
+ENV MANIFEST_REPO=
+ENV MANIFEST_BRANCH=
+ENV KUSTOMIZATION=
+
+ENTRYPOINT ["/bin/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+apk update && apk add --no-cache git && apk add --no-cache openssh
+mkdir -p ~/.ssh && echo $SSH_KEY | base64 -d > ~/.ssh/id_rsa && chmod 700 ~/.ssh/id_rsa && ssh-keyscan $MANIFEST_HOST >> ~/.ssh/known_hosts
+rm -rf $MANIFEST_REPO && git clone ssh://git@$MANIFEST_HOST/$MANIFEST_USER/$MANIFEST_REPO.git
+
+cd $MANIFEST_REPO
+git checkout $MANIFEST_BRANCH
+cd $KUSTOMIZATION
+
+for IMAGE in $(echo $IMAGES | sed "s/,/ /g")
+do
+    kustomize edit set image $IMAGE:$IMAGE_TAG
+done
+
+git config --global user.name $MANIFEST_USER
+git config --global user.email $MANIFEST_USER_EMAIL
+git add . && git commit --allow-empty -m "🚀 update to ${IMAGE_TAG}"
+git push ssh://git@$MANIFEST_HOST/$MANIFEST_USER/$MANIFEST_REPO.git
